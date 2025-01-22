7f80e5b9d076503a58776d67b7c804854a2e4c1b
awais-cheema <129386260+awais-cheema@users.noreply.github.com>
2025-01-23 00:34:56 +0500

    Mobile Responsiveness Added - Cheema

---
+++ Mobile-Header.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const navCheckbox = document.querySelector("#nav-check");
+    if (navCheckbox) {
+      navCheckbox.checked = false; // Reset the checkbox to its default (unchecked) state
+    }
+  });
+  
