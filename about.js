b1d6a3181b2a4a3ed1219ad373309de516246ade
anas-works <manasejaz.works@gmail.com>
2025-01-31 21:26:29 +0500

    about section ++

---
+++ about.js
@@ -0,0 +1,14 @@
+gsap.registerPlugin(ScrollTrigger);
+
+if(window.innerWidth > 500){
+    // gsap.to(".videoS3" ,{
+    //     scrollTrigger: {
+    //         trigger: ".s3box1",
+    //         start: "0% 90%",
+    //         end: "50% 50%",
+    //         scrub: true,
+    //         markers:true,
+    //     },
+    //     scale: 0.8,
+    // });
+}
